afacb892748be6123fd60970d9cdaf830726e97d
Evan Palmer <evjpalmer@gmail.com>
2016-04-01 18:24:45 -0700

    give dummy cars colors, remove 'White' as an option, use ng-if instead of ng-show

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@ const parking = angular.module('parking', []);
 
 parking.controller('parkingCtrl', function parkingCtrl($scope) {
   $scope.appTitle = 'Parki-ng';
-  $scope.colors = ['White', 'Black', 'Blue', 'Red', 'Silver'];
+  $scope.colors = ['Black', 'Blue', 'Red', 'Silver'];
   $scope.cars = [
-    { plate: '32JR3', dateParked: new Date(), color: 'default' },
-    { plate: '52HR4', dateParked: new Date(), color: 'default' },
-    { plate: '78UI1', dateParked: new Date(), color: 'default' }
+    { plate: '32JR3', dateParked: new Date(), color: 'Blue' },
+    { plate: '52HR4', dateParked: new Date(), color: 'Red' },
+    { plate: '78UI1', dateParked: new Date(), color: 'Black' }
   ];
   $scope.park = function (car) {
     car.dateParked = new Date();
